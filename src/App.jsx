8519661cb4229b31f882c358f7d9db59a06ac6e0
samito <martinsamuelpp@gmail.com>
2024-09-25 22:15:09 -0600

    firts excercises 1.6 to 1.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,7 @@
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Total from "./components/Total";
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+  
   return (
-    <main>
-      <Header course={course} />
-      <Content
-      course={course}
-      />
-      <Total
-      course={course}
-      />
-    </main>
+    <h2>hello</h2>
   );
 };
 
