ebdfe1b907d34aeb3fb09efe95e23ef24060a38b
samito <martinsamuelpp@gmail.com>
2024-09-27 17:32:58 -0600

    excercise 1.8, unicafe, step 3 complete

--- src/App.jsx
+++ src/App.jsx
@@ -6,49 +6,54 @@ import Average from "./components/Average";
 import PercentagePositive from "./components/PercentagePositive";
 
 const App = () => {
-
   const [value, setValue] = useState({
     good: 0,
     neutral: 0,
-    bad:0
+    bad: 0,
   });
-  
+
   const handleClickGood = () => {
-    setValue({...value, good: value.good + 1});
-  }
+    setValue({ ...value, good: value.good + 1 });
+  };
 
   const handleClickNeutral = () => {
-    setValue({...value, neutral: value.neutral + 1});
-  }
+    setValue({ ...value, neutral: value.neutral + 1 });
+  };
 
   const handleClickBad = () => {
-    setValue({...value, bad: value.bad + 1});
-  }
-  
+    setValue({ ...value, bad: value.bad + 1 });
+  };
+
   const handleReset = () => {
     setValue({
       good: 0,
       neutral: 0,
-      bad: 0
+      bad: 0,
     });
-  }
+  };
 
   return (
     <main>
-      <Title title={'Give Feedback'} />
+      <Title title={"Give Feedback"} />
       <div>
         <button onClick={handleClickGood}>Good</button>
         <button onClick={handleClickNeutral}>Neutral</button>
         <button onClick={handleClickBad}>Bad</button>
         <button onClick={handleReset}>reset</button>
       </div>
-      <Title title={'Statistics'} />
-      <div>
-        <Statistics good={value.good} neutral={value.neutral} bad={value.bad} />
-      </div>
-      <AllStatistics good={value.good} neutral={value.neutral} bad={value.bad} />
+      <Title title={"Statistics"} />
+      <Statistics good={value.good} neutral={value.neutral} bad={value.bad} />
+      <AllStatistics
+        good={value.good}
+        neutral={value.neutral}
+        bad={value.bad}
+      />
       <Average good={value.good} neutral={value.neutral} bad={value.bad} />
-      <PercentagePositive good={value.good} neutral={value.neutral} bad={value.neutral} />
+      <PercentagePositive
+        good={value.good}
+        neutral={value.neutral}
+        bad={value.neutral}
+      />
     </main>
   );
 };
