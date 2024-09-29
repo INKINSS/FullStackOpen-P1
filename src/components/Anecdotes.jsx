48a22c85eec8a06ef1250822177193a05f83abed
samito <martinsamuelpp@gmail.com>
2024-09-29 16:13:50 -0600

    funcion de votos para anecdotas individual añadido y siguiente anecdota

--- src/components/Anecdotes.jsx
+++ src/components/Anecdotes.jsx
@@ -1,24 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import VoteAnecdotes from './VoteAnecdotes';
 
 const Anecdotes = () => {
-  const [anecdotes, setAnecdotes] = useState([]);
-  const [currentAnecdote, setCurrentAnecdote] = useState('');
 
   useEffect(() => {
     fetch('../../Anecdotes.json')
       .then(response => response.json())
-      .then(data => setAnecdotes(data));
   }, []);
-
-  const showRandomAnecdote = () => {
-    const randomIndex = Math.floor(Math.random() * anecdotes.length);
-    setCurrentAnecdote(anecdotes[randomIndex].text);
-  };
 
   return (
     <div style={{margin:'10px 0'}}>
-      <button type='button' onClick={showRandomAnecdote}>Mostrar Anécdota</button>
-      {currentAnecdote && <p>{currentAnecdote}</p>}
+      <VoteAnecdotes />
     </div>
   );
 };
